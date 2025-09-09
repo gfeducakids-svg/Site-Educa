33b66db2dc0ae8c468b200c3125a147e2589b86b
Victor Dias <victordias0000000@gmail.com>
2025-09-09 20:18:37 +0000

    Deixe ele harmonico agora, ou seja deixe diminua a fonte e coloque "DESC

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -11,8 +11,8 @@ export function Hero() {
         <div className="grid gap-8 items-center justify-center">
           <div className="flex flex-col justify-center space-y-6 text-center">
             <div className="space-y-4">
-              <h1 className="text-2xl font-bold font-headline tracking-tighter sm:text-4xl md:text-5xl text-foreground">
-                Descubra Qual o APP Chinês que alfabetiza crianças com apenas 15 minutos por dia! <span className="text-primary">(3x mais rápido)</span>
+              <h1 className="text-2xl font-bold font-headline tracking-tighter sm:text-3xl md:text-4xl text-foreground">
+                DESCUBRA Qual o APP Chinês que alfabetiza crianças com apenas 15 minutos por dia! <span className="text-primary">(3x mais rápido)</span>
               </h1>
               <p className="max-w-[600px] text-muted-foreground md:text-xl font-rubik mx-auto">
                 Para pais e educadores que querem alfabetizar sem lágrimas ou pressão em apenas 30 dias - usado por +1.200 famílias.
