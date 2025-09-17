af7558732a6bdd42ee0116904d27560cd59e794a
Victor Dias <victordias0000000@gmail.com>
2025-09-17 19:03:36 +0000

    Deixe igual a fonte do "ECONOMIZE R$ 102,00" (_for element p_)

--- src/components/landing/pricing.tsx
+++ src/components/landing/pricing.tsx
@@ -90,7 +90,7 @@ export function Pricing() {
                 <p className="text-base text-gray-500 line-through">
                   VALOR NORMAL: R$ 149,90
                 </p>
-                <p className="text-2xl font-bold text-green-600 my-1 animate-pulse">
+                <p className="text-base font-semibold text-amber-800 mt-4">
                   HOJE : R$ 47,90
                 </p>
                 <p className="text-sm text-gray-500">Apenas R$ 1,60 por dia durante o primeiro mês</p>
